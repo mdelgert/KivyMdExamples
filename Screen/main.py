2d5ff4755ab431d71654f6aae9c05fb6849bb98d
Matthew Elgert <mdelgert@yahoo.com>
2023-08-18 14:29:32 -0400

    Add base screen

---
+++ Screen/main.py
@@ -0,0 +1,29 @@
+# https://kivymd.readthedocs.io/en/latest/themes/theming/
+
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+
+# Load the kv files for each screen
+Builder.load_file("screen1.kv")
+Builder.load_file("screen2.kv")
+Builder.load_file("screen3.kv")
+
+# Define the screen classes
+class Screen1(Screen):
+    pass
+
+class Screen2(Screen):
+    pass
+
+class Screen3(Screen):
+    pass
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.material_style = "M2"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_file("main.kv")
+
+MainApp().run()
