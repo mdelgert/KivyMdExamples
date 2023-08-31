774e12865039afb15ae60c182480b574abc624dd
Matthew Elgert <mdelgert@yahoo.com>
2023-08-30 20:58:18 -0400

    add pre enter

---
+++ PreEnter/main.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class MainScreen(Screen):
+    def on_pre_enter(self):
+        print("Main pre enter")
+        self.ids.main_label.text = 'Home screen text updated.'       
+
+class SettingsScreen(Screen):
+    def on_pre_enter(self):
+        print("Settings pre enter")
+        self.ids.settings_label.text = 'Settings screen text updated.'         
+
+class MyApp(App):
+    def build(self):
+        self.sm = ScreenManager()
+        self.sm.add_widget(MainScreen(name='main'))
+        self.sm.add_widget(SettingsScreen(name='settings'))
+        return self.sm
+
+    def settings_screen_opened(self):
+        print("Settings screen opened")
+
+if __name__ == '__main__':
+    Builder.load_file('main.kv')
+    Builder.load_file('settings.kv')
+    MyApp().run()
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# KivyMdExamples
+### KivyMdExamples
 https://github.com/kivymd/KivyMD/wiki/Modules-Material-App
 https://github.com/kivymd/KivyMD
 https://kivymd.readthedocs.io/en/latest/
 
-# Install
+### Install
 pip install kivymd==1.1.1
+
+### Examples
+https://github.com/MariyaSha/BinarytoDecimal.git
+https://github.com/attreyabhatt/KivyMD-Basics.git
+https://github.com/kivymd/KitchenSink.git
+https://github.com/Dirk-Sandberg/FarmersMarketFinderTutorial.git
+https://github.com/haddiebakrie/WhatsApp-Redesign.git
+https://github.com/HusamFathi/KivyMD-Dashboard-UI.git
+https://github.com/coreproject-moe/android-kivy.git
+https://github.com/Spidy20/Python_To_APK.git
