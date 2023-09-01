3e677f3b4d7d43f6fdb7271601f538568f3893c3
Matthew Elgert <mdelgert@yahoo.com>
2023-09-01 13:37:14 -0400

    add md nav

---
+++ ScreenManagerMdNav/main.py
@@ -0,0 +1,51 @@
+# https://kivymd.readthedocs.io/en/latest/themes/theming/
+
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivy.uix.popup import Popup
+
+# Load the kv files for each screen
+for screen_name in [
+    "master",
+    "home",
+    "about",
+    "contact",
+    "popup"
+    ]:
+    
+    Builder.load_file(f"screens/{screen_name}.kv")
+
+# Define the screen classes
+class Home(Screen):
+    def show_popup(self):
+        print("show_popup pressed")
+        popup = MyPopup()
+        popup.open()
+
+class MyPopup(Popup):
+    def dismiss_popup(self):
+        self.dismiss()  # Dismiss the popup
+    
+    def btn_hello(self):
+        print("btn_home pressed")
+        self.ids.pop_label.text = "Hello Pop!"
+
+class About(Screen):
+    pass
+
+class Contact(Screen):
+    pass
+
+class Master(BoxLayout):
+    pass
+
+class MainApp(MDApp):
+    def build(self):
+        self.root = Master()
+        self.theme_cls.material_style = "M2"
+        self.theme_cls.theme_style = "Dark"
+        return self.root
+
+MainApp().run()
