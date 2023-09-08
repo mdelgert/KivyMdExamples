effdfd2ff2b6c567a48159f55e9bfee4fa31163e
Matthew Elgert <mdelgert@yahoo.com>
2023-09-08 02:48:35 -0400

    update nav2

--- ScreenManagerMdNav2/main.py
+++ ScreenManagerMdNav2/main.py
@@ -1,6 +1,9 @@
+#https://stackoverflow.com/questions/68854618/how-to-use-kivymd-bottom-navigation-to-switch-between-multiple-screens
+
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
 
 for screen_name in [
     "master",
@@ -12,30 +15,44 @@ for screen_name in [
     
     Builder.load_file(f"{screen_name}.kv")
 
-class Master(MDScreen):
+class Master(Screen):
     pass
 
-class Home(MDScreen):
+class Home(Screen):
     def btn_question(self):
-        print("Home: btn_question")
+        print("btn_question")
         #self.root.ids.screen_manager.current = "question"
-        self.parent.parent.transition.direction = 'left'
-        self.parent.parent.current = 'question'
+        #self.parent.parent.transition.direction = 'left'
+        #self.parent.parent.current = 'question'
         
-class About(MDScreen):
+class About(Screen):
     pass
 
-class Contact(MDScreen):
+class Contact(Screen):
     pass
 
-class Question(MDScreen):
+class Question(Screen):
     pass
 
 class MainApp(MDApp):
+    # def build(self):
+    #     self.root = Master()
+    #     self.theme_cls.material_style = "M2"
+    #     self.theme_cls.theme_style = "Dark"
+    #     return self.root
+
     def build(self):
-        self.root = Master()
-        self.theme_cls.material_style = "M2"
-        self.theme_cls.theme_style = "Dark"
-        return self.root
+        self.sm = ScreenManager(transition=NoTransition())
+        self.sm.add_widget(Master(name="master"))
+        self.sm.add_widget(Home(name="home"))
+        self.sm.add_widget(About(name="about"))
+        self.sm.add_widget(Contact(name="contact"))
+        self.sm.add_widget(Question(name="question"))
+        #self.sm.current = 'question'
+        return self.sm
+
+    def change_screen(self, screen):
+        print("change_screen")
+        self.sm.current = screen
 
 MainApp().run()
--- ScreenManagerMdNav3/main.py
+++ ScreenManagerMdNav3/main.py
@@ -1,8 +1,6 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationItem
-from kivymd.uix.button import MDRaisedButton
 
 # Create HomeScreen class (unchanged)
 class HomeScreen(MDScreen):
@@ -23,17 +21,7 @@ class MainApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.theme_style = "Light"
-
-        # Check a condition to hide the "Question" screen
-        if not self.should_show_question_screen():
-            self.root.ids.bottom_navigation.remove_widget(self.root.ids.screen_question)
-        
-        return Builder.load_file("main.kv")
-
-    def should_show_question_screen(self):
-        # Add your condition to determine whether to show the "Question" screen
-        # For example, you can return True to always show it, or False to hide it
-        return True
+        #return Builder.load_file("main.kv")
 
 if __name__ == "__main__":
     MainApp().run()
